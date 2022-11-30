bbd83d87486ff0e1ab2bacabfd07f5c8fc18a7a9
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-11-30 21:59:52 +0530

    add database

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"short-leaf/app/config/database"
 	"short-leaf/app/routes"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,11 +11,13 @@ import (
 
 func main() {
 
+	database.DatabaseSetup()
+
 	fmt.Println("Starting Servcer")
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello From Go")
 	})
 	routes.AuthRoutes(app)
-	app.Listen(":3000")
+	log.Fatal(app.Listen(":3000"))
 }
